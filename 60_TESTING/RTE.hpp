4c40a5b9ef55651488f395c7f68588a038aa7c19
Nievillis <dgtroll.schoosi@gmail.com>
2022-11-13 23:59:02 +0100

    DManager + ConsoleEngine includion

---
+++ 60_TESTING/RTE.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+#include <DUNE/DControlManager.hpp>
+
+namespace RTE
+{
+    inline extern DManager::DControlManager *dcm_ptr = nullptr;
+    inline extern int millis = 0;
+};
--- 60_TESTING/main.cpp
+++ 60_TESTING/main.cpp
@@ -17,7 +17,8 @@
 |* INCLUDE
 |***********************************/
 
-#include "room.hpp"
+#include <consoleengine/ConsoleEngine.hpp>
+#include "RTE.hpp"
 
 /************************************
 |* MAIN fct
@@ -27,6 +28,71 @@
 |***********************************/
 int main(void)
 {
-    GTC::room r;
+    {
+        ConsoleEngine ce(std::cout);
+        ConsoleEngine::_set_context(ce);
+    }
+
+    DManager::DControlManager dcm;
+    RTE::dcm_ptr = &dcm;
+
+    class : DManager::DManager {
+        using DManager::DManager::DManager;
+
+        virtual void init_user() override
+        {
+        }
+
+        volatile int count = 0;
+
+        virtual void run_user() override
+        {
+            count++;
+            ConsoleEngine_context.clear();
+            ConsoleEngine_context.add_string("Count: ", Pixel::DEFAULT, Pixel::TEXT, Pixel::BOLD)
+                .add_string(std::to_string(count), Pixel::YELLOW, Pixel::TEXT, Pixel::UNDERLINED);
+
+            if(count > 10000)
+            {
+                RTE::dcm_ptr->stop();
+            }
+        }
+
+    } mgr_stack(DManager::DManager::DManagerFlags{
+        .can_skip               = true,    
+        .log_lag                = true,    
+        .target_period          = std::chrono::milliseconds(2),        
+        .delay_target_period    = std::chrono::milliseconds(0),                
+        .log_ela                = false,    
+        .name                   = "Test Manager",
+        .adress_millis          = &RTE::millis        
+    });
+
+    class : DManager::DManager {
+        using DManager::DManager::DManager;
+
+        virtual void init_user() override
+        {
+        }
+
+        virtual void run_user() override
+        {
+            std::cout << ConsoleEngine_context.add_string(std::string(" Elapsation: ") + std::to_string(RTE::millis) + "ms", Pixel::BLACK);
+        }
+
+    } mgr_print(DManager::DManager::DManagerFlags{
+        .can_skip               = true,    
+        .log_lag                = true,    
+        .target_period          = std::chrono::milliseconds(10),        
+        .delay_target_period    = std::chrono::milliseconds(0),                
+        .log_ela                = false,    
+        .name                   = "Test Manager",
+        .adress_millis          = nullptr        
+    });
+
+
+    dcm.subscribe(&mgr_stack);
+    dcm.subscribe(&mgr_print);
+    dcm.launch();
 }
 
