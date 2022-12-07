dceb7904bf993f20de2d6c8dc84422e94e227b8e
Nievillis <dgtroll.schoosi@gmail.com>
2022-12-07 13:20:39 +0100

    Removed old stuff

--- 60_TESTING/Room.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <consoleengine/ConsoleEngine.hpp>
-
-class Room
-{
-    const pixelstr ba
-};
--- 61_TESTING/config.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define GRAPHICS_PER_SEC    (10.0)
-#define PHYSICS_PER_SEC     (20.0)
